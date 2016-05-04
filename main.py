a0a79c47d365f015a860864784b58b1350893173
haghighi <haghighi.ahmad@gmail.com>
2016-05-05 01:34:06 +0430

    main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import urllib
-params = urllib.urlencode({'spam': 1, 'eggs': 2, 'bacon': 0})
+params = urllib.urlencode({'spam': 1, 'eggs': 2})
 f = urllib.urlopen("http://www.google.com/?%s" % params)
 print f.read()
