a055aa39d6adf1bfa0de4a260b0b0f7b459a3147
haghighi <haghighi.ahmad@gmail.com>
2016-05-05 01:04:41 +0430

    main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import urllib
 params = urllib.urlencode({'spam': 1, 'eggs': 2, 'bacon': 0})
-f = urllib.urlopen("http://www.musi-cal.com/cgi-bin/query?%s" % params)
+f = urllib.urlopen("http://www.google.com/?%s" % params)
 print f.read()
