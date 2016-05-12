f539dff97b7a86a3b6115565908ba241000ea106
haghighi <haghighi.ahmad@gmail.com>
2016-05-12 20:26:54 +0430

    ss

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import urllib
+from bs4 import BeautifulSoup
+
 params = urllib.urlencode({'spam': 1, 'eggs': 2})
-f = urllib.urlopen("http://www.google.com/?%s" % params)
+f = urllib.urlopen("http://localhost/?%s" % params)
+
 print f.read()
